525f462356da6b19740608ff13ff097bd57a23f3
Srojiddin <152707481+Srojiddin@users.noreply.github.com>
2024-02-06 00:30:40 +0600

    first commit

---
+++ apps/products/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from apps.products.models import Product, ProductImage
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['image']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+    list_filter = ['category']
+    search_fields = ['title']
+
---
+++ utils/image_path.py
@@ -0,0 +1,12 @@
+import os
+
+
+def upload_avatar_for_user(instance, filename):
+    return f"/avatars/{instance.username}/{filename}"
+
+
+def upload_products(instance, filename):
+    return f"/products/{instance.product.title}/{filename}"
+
+
+
