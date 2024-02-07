893e9ec09f85bc6ecdb06269713d5f2545742f35
Srojiddin <152707481+Srojiddin@users.noreply.github.com>
2024-02-08 02:23:24 +0600

    dastan

--- core/settings.py
+++ core/settings.py
@@ -108,13 +108,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+#
+# STATIC_DIR = '/static/'
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [STATIC_DIR]
+#
+# MEDIA_URL = '/static/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_DIR = '/static/'
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_DIR]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_DIR = BASE_DIR / 'static'
+MEDIA_URL ='media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
